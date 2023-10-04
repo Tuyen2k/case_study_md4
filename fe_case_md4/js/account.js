6053aa771ea2ddaa82ea557b73d8943a3a4cec18
Tuyen2k <tuyen4835@gmail.com>
2023-10-04 17:23:53 +0700

    display account fe

---
+++ fe_case_md4/js/account.js
@@ -0,0 +1,36 @@
+class Account {
+    id
+    name
+    email
+    address_delivery
+    delete
+    role
+}
+
+function getAllAccount() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/auth",
+        success: function (data) {
+            console.log(data);
+            displayAccount(data);
+        }
+    })
+}
+
+function displayAccount(data) {
+    let content = "";
+    for (let i = 0; i < data.length; i++) {
+        content += `<tr>
+                          <td class="align-middle">${data[i].name}</td> 
+                          <td class="align-middle">${data[i].email}</td>
+                          <td class="align-middle">
+                            ${data[i].role.name}
+                         </td>
+                         <td class="align-middle"><button class="btn btn-sm btn-primary"><i class="fa fa-times"></i></button>
+                         </td>
+                 </tr>`
+    }
+    document.getElementById("display_account").innerHTML = content;
+}
+
