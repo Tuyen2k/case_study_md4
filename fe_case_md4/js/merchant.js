43171db902be68749030f95f66410fa2bd3e70bc
Tuyen2k <tuyen4835@gmail.com>
2023-10-04 22:06:34 +0700

    teamplate login, register

--- fe_case_md4/js/merchant.js
+++ fe_case_md4/js/merchant.js
@@ -1,4 +1,31 @@
 
 function getAllMerchant(){
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/merchant",
+        success: function (data){
+            displayMerchant(data)
+        }
+    })
+}
 
+function displayMerchant(data){
+    let content = "";
+    for (let i = 0; i < data.length; i++) {
+        content += `<tr> <td class="align-middle">${i + 1}</td> 
+                          <td class="align-middle">${data[i].name}</td> 
+                          <td class="align-middle">${data[i].email}</td>
+                          <td class="align-middle">${data[i].phone}</td>
+                          <td class="align-middle">${data[i].address_shop.city.name}, 
+${data[i].address_shop.district.name}, ${data[i].address_shop.ward.name}, ${data[i].address_shop.address_detail}</td> 
+                      
+                         <td class="align-middle"><button class="btn btn-sm btn-primary"><i class="fa fa-check"></i></button>
+                         <button class="btn btn-sm btn-primary"><i class="fa fa-times"></i></button>
+                         </td>
+                          <td class="align-middle">
+                         <button class="btn btn-sm btn-primary"><i class="fa fa-times"></i></button>
+                         </td>
+                 </tr>`
+    }
+    document.getElementById("display_merchant").innerHTML = content;
 }
