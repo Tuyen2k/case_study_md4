3e7592b491eed23189f34286c0bfbc9868a198c4
danhthai3 <ndthai2001@gmail.com>
2023-10-05 08:58:46 +0700

    be category

--- src/main/java/com/example/case_md4/controller/CategoryController.java
+++ src/main/java/com/example/case_md4/controller/CategoryController.java
@@ -1,11 +1,40 @@
 package com.example.case_md4.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.case_md4.model.Category;
+import com.example.case_md4.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/categories")
 public class CategoryController {
+    @Autowired
+    private ICategoryService iCategoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategory() {
+        return new ResponseEntity<>(iCategoryService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        iCategoryService.save(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Void> updateCategory(@RequestBody Category category) {
+        iCategoryService.save(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(iCategoryService.findById(id));
+    }
 }
---
+++ src/main/java/com/example/case_md4/service/ICategoryService.java
@@ -0,0 +1,6 @@
+package com.example.case_md4.service;
+
+import com.example.case_md4.model.Category;
+
+public interface ICategoryService extends IGenerateService<Category> {
+}
